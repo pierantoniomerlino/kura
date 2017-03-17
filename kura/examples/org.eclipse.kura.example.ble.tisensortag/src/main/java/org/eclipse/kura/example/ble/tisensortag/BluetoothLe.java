81d2dac4de63790da39132f4ef4f826a1f70ee6d
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2017-03-17 16:49:52 +0100

    Fixed bugs
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/examples/org.eclipse.kura.example.ble.tisensortag/src/main/java/org/eclipse/kura/example/ble/tisensortag/BluetoothLe.java
+++ kura/examples/org.eclipse.kura.example.ble.tisensortag/src/main/java/org/eclipse/kura/example/ble/tisensortag/BluetoothLe.java
@@ -60,7 +60,7 @@ public class BluetoothLe implements ConfigurableComponent, CloudClientListener, 
     private final String PROPERTY_INAME = "iname";
 
     private CloudService cloudService;
-    private CloudClient cloudClient;
+    private static CloudClient cloudClient;
     private List<TiSensorTag> tiSensorTagList;
     private BluetoothService bluetoothService;
     private BluetoothAdapter bluetoothAdapter;
@@ -70,9 +70,9 @@ public class BluetoothLe implements ConfigurableComponent, CloudClientListener, 
 
     private int period = 10;
     private int scantime = 5;
-    private String topic = "data";
+    private static String topic = "data";
     private long startTime;
-    private boolean connected = false;
+    // private boolean connected = false;
     private String iname = "hci0";
     private boolean enableScan = false;
     private boolean enableTemp = false;
@@ -141,7 +141,7 @@ public class BluetoothLe implements ConfigurableComponent, CloudClientListener, 
                         logger.info("Bluetooth adapter address => " + this.bluetoothAdapter.getAddress());
                     }
                     this.startTime = 0;
-                    this.connected = false;
+                    // this.connected = false;
                     this.handle = this.worker.scheduleAtFixedRate(new Runnable() {
 
                         @Override
@@ -245,7 +245,7 @@ public class BluetoothLe implements ConfigurableComponent, CloudClientListener, 
                         logger.info("Bluetooth adapter address => " + this.bluetoothAdapter.getAddress());
                     }
                     this.startTime = 0;
-                    this.connected = false;
+                    // this.connected = false;
                     this.handle = this.worker.scheduleAtFixedRate(new Runnable() {
 
                         @Override
@@ -348,7 +348,7 @@ public class BluetoothLe implements ConfigurableComponent, CloudClientListener, 
         }
     }
 
-    protected void doPublishKeys(String address, Object key) {
+    protected static void doPublishKeys(String address, Object key) {
         KuraPayload payload = new KuraPayload();
         payload.setTimestamp(new Date());
         payload.addMetric("key", key);
@@ -479,13 +479,13 @@ public class BluetoothLe implements ConfigurableComponent, CloudClientListener, 
             Boolean connected;
             if (!myTiSensorTag.isConnected()) {
                 logger.info("Connecting to TiSensorTag...");
-                this.connected = myTiSensorTag.connect(this.iname);
+                connected = myTiSensorTag.connect(this.iname);
             } else {
                 logger.info("TiSensorTag already connected!");
-                this.connected = true;
+                connected = true;
             }
 
-            if (this.connected) {
+            if (connected) {
 
                 KuraPayload payload = new KuraPayload();
                 payload.setTimestamp(new Date());
