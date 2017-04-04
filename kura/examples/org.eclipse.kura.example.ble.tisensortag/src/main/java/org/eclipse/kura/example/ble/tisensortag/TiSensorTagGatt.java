42b07b7e24222a4e6df5e03472ccbac523ddb421
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2017-04-04 15:05:29 +0200

    A lot of fixes and modifications
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/examples/org.eclipse.kura.example.ble.tisensortag/src/main/java/org/eclipse/kura/example/ble/tisensortag/TiSensorTagGatt.java
+++ kura/examples/org.eclipse.kura.example.ble.tisensortag/src/main/java/org/eclipse/kura/example/ble/tisensortag/TiSensorTagGatt.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Eurotech and/or its affiliates
+ * Copyright (c) 2011, 2017 Eurotech and/or its affiliates
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -47,6 +47,7 @@ public class TiSensorTagGatt {
     public static final String HANDLE_ACC_SENSOR_ENABLE_2541 = "0x0031";
     public static final String HANDLE_ACC_SENSOR_PERIOD_2541 = "0x0034";
 
+    public static final UUID UUID_ACC_SENSOR_SERVICE = UUID.fromString("f000aa10-0451-4000-b000-000000000000");
     public static final UUID UUID_ACC_SENSOR_VALUE = UUID.fromString("f000aa11-0451-4000-b000-000000000000");
     public static final UUID UUID_ACC_SENSOR_ENABLE = UUID.fromString("f000aa12-0451-4000-b000-000000000000");
     public static final UUID UUID_ACC_SENSOR_PERIOD = UUID.fromString("f000aa13-0451-4000-b000-000000000000");
@@ -62,6 +63,7 @@ public class TiSensorTagGatt {
     public static final String HANDLE_HUM_SENSOR_ENABLE_2650 = "0x002c";
     public static final String HANDLE_HUM_SENSOR_PERIOD_2650 = "0x002E";
 
+    public static final UUID UUID_HUM_SENSOR_SERVICE = UUID.fromString("f000aa20-0451-4000-b000-000000000000");
     public static final UUID UUID_HUM_SENSOR_VALUE = UUID.fromString("f000aa21-0451-4000-b000-000000000000");
     public static final UUID UUID_HUM_SENSOR_ENABLE = UUID.fromString("f000aa22-0451-4000-b000-000000000000");
     public static final UUID UUID_HUM_SENSOR_PERIOD = UUID.fromString("f000aa23-0451-4000-b000-000000000000");
@@ -73,6 +75,7 @@ public class TiSensorTagGatt {
     public static final String HANDLE_MAG_SENSOR_ENABLE_2541 = "0x0044";
     public static final String HANDLE_MAG_SENSOR_PERIOD_2541 = "0x0047";
 
+    public static final UUID UUID_MAG_SENSOR_SERVICE = UUID.fromString("f000aa30-0451-4000-b000-000000000000");
     public static final UUID UUID_MAG_SENSOR_VALUE = UUID.fromString("f000aa31-0451-4000-b000-000000000000");
     public static final UUID UUID_MAG_SENSOR_ENABLE = UUID.fromString("f000aa32-0451-4000-b000-000000000000");
     public static final UUID UUID_MAG_SENSOR_PERIOD = UUID.fromString("f000aa33-0451-4000-b000-000000000000");
@@ -94,6 +97,7 @@ public class TiSensorTagGatt {
     public static final String HANDLE_PRE_SENSOR_ENABLE_2650 = "0x0034";
     public static final String HANDLE_PRE_SENSOR_PERIOD_2650 = "0x0036";
 
+    public static final UUID UUID_PRE_SENSOR_SERVICE = UUID.fromString("f000aa40-0451-4000-b000-000000000000");
     public static final UUID UUID_PRE_SENSOR_VALUE = UUID.fromString("f000aa41-0451-4000-b000-000000000000");
     public static final UUID UUID_PRE_SENSOR_ENABLE = UUID.fromString("f000aa42-0451-4000-b000-000000000000");
     public static final UUID UUID_PRE_SENSOR_CALIBRATION = UUID.fromString("f000aa43-0451-4000-b000-000000000000");
@@ -105,6 +109,7 @@ public class TiSensorTagGatt {
     public static final String HANDLE_GYR_SENSOR_NOTIFICATION_2541 = "0x0058";
     public static final String HANDLE_GYR_SENSOR_ENABLE_2541 = "0x005b";
 
+    public static final UUID UUID_GYR_SENSOR_SERVICE = UUID.fromString("f000aa50-0451-4000-b000-000000000000");
     public static final UUID UUID_GYR_SENSOR_VALUE = UUID.fromString("f000aa51-0451-4000-b000-000000000000");
     public static final UUID UUID_GYR_SENSOR_ENABLE = UUID.fromString("f000aa52-0451-4000-b000-000000000000");
 
@@ -116,6 +121,7 @@ public class TiSensorTagGatt {
     public static final String HANDLE_KEYS_STATUS_2650 = "0x0049";
     public static final String HANDLE_KEYS_NOTIFICATION_2650 = "0x004A";
 
+    public static final UUID UUID_KEYS_SERVICE = UUID.fromString("f000ffe0-0451-4000-b000-000000000000");
     public static final UUID UUID_KEYS_STATUS = UUID.fromString("f000ffe1-0451-4000-b000-000000000000");
 
     // Ambient Light sensor
@@ -125,6 +131,7 @@ public class TiSensorTagGatt {
     public static final String HANDLE_OPTO_SENSOR_ENABLE_2650 = "0x0044";
     public static final String HANDLE_OPTO_SENSOR_PERIOD_2650 = "0x0046";
 
+    public static final UUID UUID_OPTO_SENSOR_SERVICE = UUID.fromString("f000aa70-0451-4000-b000-000000000000");
     public static final UUID UUID_OPTO_SENSOR_VALUE = UUID.fromString("f000aa71-0451-4000-b000-000000000000");
     public static final UUID UUID_OPTO_SENSOR_ENABLE = UUID.fromString("f000aa72-0451-4000-b000-000000000000");
     public static final UUID UUID_OPTO_SENSOR_PERIOD = UUID.fromString("f000aa73-0451-4000-b000-000000000000");
@@ -136,6 +143,7 @@ public class TiSensorTagGatt {
     public static final String HANDLE_MOV_SENSOR_ENABLE_2650 = "0x003C";
     public static final String HANDLE_MOV_SENSOR_PERIOD_2650 = "0x003E";
 
+    public static final UUID UUID_MOV_SENSOR_SERVICE = UUID.fromString("f000aa80-0451-4000-b000-000000000000");
     public static final UUID UUID_MOV_SENSOR_VALUE = UUID.fromString("f000aa81-0451-4000-b000-000000000000");
     public static final UUID UUID_MOV_SENSOR_ENABLE = UUID.fromString("f000aa82-0451-4000-b000-000000000000");
     public static final UUID UUID_MOV_SENSOR_PERIOD = UUID.fromString("f000aa83-0451-4000-b000-000000000000");
@@ -145,6 +153,7 @@ public class TiSensorTagGatt {
     public static final String HANDLE_IO_SENSOR_VALUE_2650 = "0x004E";
     public static final String HANDLE_IO_SENSOR_ENABLE_2650 = "0x0050";
 
+    public static final UUID UUID_IO_SENSOR_SERVICE = UUID.fromString("f000aa64-0451-4000-b000-000000000000");
     public static final UUID UUID_IO_SENSOR_VALUE = UUID.fromString("f000aa65-0451-4000-b000-000000000000");
     public static final UUID UUID_IO_SENSOR_ENABLE = UUID.fromString("f000aa66-0451-4000-b000-000000000000");
 }
